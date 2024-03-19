3078166529b9961caa2dac97211cdb4a62cc4b2f
Antti Varis <ajvari88@gmail.com>
2024-03-19 21:17:48 +0200

    osa1 teh1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="Good" clickAction={() => setGood(good + 1)} />
+      <Button text="Neutral" clickAction={() => setNeutral(neutral + 1)} />
+      <Button text="Bad" clickAction={() => setBad(bad + 1)} />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({clickAction, text}) => <button onClick={clickAction}>{text}</button>
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
