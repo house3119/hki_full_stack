7b1535bdd70333925c07faafe7c0bc2e8e9bd020
Antti Varis <ajvari88@gmail.com>
2024-03-19 21:35:43 +0200

    osa1 teh1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -21,13 +21,27 @@ const App = () => {
 const Button = ({clickAction, text}) => <button onClick={clickAction}>{text}</button>
 
 const Statistics = ({good, neutral, bad}) => {
+  let total = good + neutral + bad
+
+  let average = "-"
+  let positive = "-"
+  if (total != 0) {
+    average = (good + bad * (-1)) / total
+    positive = (good / total * 100) + "%"
+  }
+
   return (
     <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Stat name="Good" value={good} />
+      <Stat name="Neutral" value={neutral} />
+      <Stat name="Bad" value={bad} />
+      <Stat name="Total" value={total} />
+      <Stat name="Average" value={average} />
+      <Stat name="Positive" value={positive} />
     </div>
   )
 }
+
+const Stat = ({name, value}) => <p>{name}: {value}</p>
 
 export default App
