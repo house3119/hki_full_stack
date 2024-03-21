ed51a470da55e4ed06d8a479a1f38f47eec700b1
Antti Varis <ajvari88@gmail.com>
2024-03-21 20:35:41 +0200

    osa2 teh2.1

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,50 @@
+const App = () => {
+  const course1 = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course1} />
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header header={ course.name } />
+      <Content parts={ course.parts }/>
+    </div>
+  )
+}
+
+const Header = ({ header }) => <h1>{ header }</h1>
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <p key={ part.id }>{part.name} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+export default App
