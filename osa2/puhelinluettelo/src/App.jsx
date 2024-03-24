2a4e8f321c26a07b65a88a42f4e7ade0bbbd6ac8
Antti Varis <ajvari88@gmail.com>
2024-03-24 21:15:15 +0200

    teh2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Number name={person.name} key={person.name} />)}
+    </div>
+  )
+}
+
+const Number = ({ name, key }) => <p key={key}>{name}</p>
+
+export default App
