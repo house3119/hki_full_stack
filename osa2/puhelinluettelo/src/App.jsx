daa5a64653e09fa2807c5332652c008651665d6a
Antti Varis <ajvari88@gmail.com>
2024-03-26 23:26:56 +0200

    teh2.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1231244'
-    }
-  ]) 
+  const all_persons = [
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]
+  const [persons, setPersons] = useState(all_persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterApplied, setFilterApplied] = useState(false)
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -18,7 +21,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleClick = (event) => {
+  const handleFilter = (event) => {
+    if (event.target.value == '') {
+      setFilterApplied(false)
+    } else {
+      setFilterApplied(true)
+      setFilter(event.target.value)
+    }
+  }
+
+  const personsToShow = filterApplied
+    ? persons.map(person => {
+      if (person.name.toLowerCase().includes(filter)) {
+        return person
+      }
+    }).filter((element) => {
+      return element
+    })
+    : persons
+
+  function handleClick(event) {
     event.preventDefault()
     const nameList = persons.map(person => person.name.toLowerCase())
 
@@ -37,20 +59,27 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Filter</h2>
       <form>
         <div>
-          name: <input onChange={handleNameChange} value={newName} />
+          Filter<input onChange={handleFilter}/>
+        </div>
+      </form>
+      <h2>Add new</h2>
+      <form>
+        <div>
+          Name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
-          number: <input onChange={hanleNumberChange} value={newNumber} />
+          Number: <input onChange={hanleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {personsToShow.map(person =>
         <Number
           name = {person.name}
           key = {person.name}
