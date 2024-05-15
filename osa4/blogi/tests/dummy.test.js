0ca5c9da46ff43409f77abd799222a5323f4e508
Antti Varis <ajvari88@gmail.com>
2024-05-15 22:38:46 +0300

    teh4.4

--- osa4/blogi/tests/dummy.test.js
+++ osa4/blogi/tests/dummy.test.js
@@ -2,9 +2,10 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+test('dummy returns one', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+
+    assert.strictEqual(result, 1)
 })
--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -1,8 +1,29 @@
-
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogList) => {
+
+    // Check for empty list
+    if (blogList.length === 0) {
+        return 0
+    }
+
+    // Add likes recursively, parameters: single blog object and it's location in the list
+    const addLikesRecursive = (singleBlog, location) => {
+        if ((location + 1) === blogList.length) {
+            return singleBlog.likes
+        } else {
+            return singleBlog.likes + addLikesRecursive(blogList[location + 1], location + 1)
+        }
+    }
+
+    // Start the call stack
+    return addLikesRecursive(blogList[0], 0)
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
