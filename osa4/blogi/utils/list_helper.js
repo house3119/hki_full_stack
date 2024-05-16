e27228a34e5505b0730d131020b56d11b56bee44
Antti Varis <ajvari88@gmail.com>
2024-05-16 18:41:21 +0300

    teh4.5

--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -2,8 +2,8 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogList) => {
 
+const totalLikes = (blogList) => {
     // Check for empty list
     if (blogList.length === 0) {
         return 0
@@ -23,7 +23,23 @@ const totalLikes = (blogList) => {
 }
 
 
+const favoriteBlog = (blogList) => {
+    // Check for empty list
+    if (blogList.length === 0) {
+        return 'empty list'
+    }
+
+    // Define callback
+    const returnBigger = (biggest, current) => {
+        return (current.likes > biggest.likes)? current : biggest
+    }
+
+    return blogList.reduce(returnBigger, blogList[0])
+}
+
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
