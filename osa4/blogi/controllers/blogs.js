bbbd2d2e05579788eec56b679e74599db92f61d7
Antti Varis <ajvari88@gmail.com>
2024-06-03 22:06:51 +0300

    teh4.18 & teh4.19

--- osa4/blogi/controllers/blogs.js
+++ osa4/blogi/controllers/blogs.js
@@ -2,6 +2,17 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 
 blogRouter.get('/', async (request, response, next) => {
@@ -11,23 +22,34 @@ blogRouter.get('/', async (request, response, next) => {
 
 
 blogRouter.post('/', async (request, response, next) => {
-    const body = request.body
-    const user = await User.findById(body.userId)
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    })
+    try {
+        const body = request.body
 
-    if (!blog.title || !blog.url) {
-        response.status(400).send({message: "Blog title or URL missing"})
-    } else {
-        const result = await blog.save()
-        user.blogs = user.blogs.concat(result._id)
-        await user.save()
-        response.status(201).json(result)
+        const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+        if (!decodedToken.id) {
+          return response.status(401).json({ error: 'token invalid' })
+        }
+        const user = await User.findById(decodedToken.id)
+    
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+        })
+    
+        if (!blog.title || !blog.url) {
+            response.status(400).send({message: "Blog title or URL missing"})
+        } else {
+            const result = await blog.save()
+            user.blogs = user.blogs.concat(result._id)
+            await user.save()
+            response.status(201).json(result)
+        }
+
+    } catch (error) {
+        next(error)
     }
 })
 
