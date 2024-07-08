a005bc6f1c16d9c73d2367edeed98341463d13f6
Antti Varis <ajvari88@gmail.com>
2024-07-08 17:34:39 +0300

    teh5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Rendered Blog component contains correct title', () => {
@@ -19,5 +20,31 @@ test('Rendered Blog component contains correct title', () => {
   />)
 
   const element = screen.getByText('Test Blog 1 - by Test Writer 1')
+})
+
+test('URL, likes and user are shown correctly after view button clicked', async () => {
+  const mockFunction = () => {
+    console.log('this is a mock function')
+  }
+
+  render(<Blog
+    id='12345'
+    title='Test Blog 2'
+    author='Test Writer 2'
+    url='www.test.org'
+    likes={1}
+    user='Tepi Testikäyttäjä'
+    likeBlog={mockFunction}
+    addedByCurrentLoggedInUser='true'
+    removeBlog={mockFunction}
+  />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const urlElement = screen.getByText('Url: www.test.org')
+  const likesElement = screen.getByText('Likes: 1')
+  const userElement = screen.getByText('User: Tepi Testikäyttäjä')
 
 })
