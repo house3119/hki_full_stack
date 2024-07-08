875098869baa836de61f1d3866e43c7ec3776481
Antti Varis <ajvari88@gmail.com>
2024-07-08 17:52:04 +0300

    teh5.15

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react'
+import { beforeEach, expect } from 'vitest'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('Rendered Blog component contains correct title', () => {
-  const mockFunc = () => {
-    console.log('test func')
-  }
+// Render same test Blog before each test
+beforeEach(() => {
 
+})
+
+test('Rendered Blog component contains correct title', () => {
+  const likeMockHandler = vi.fn()
+  const removeMockHandler = vi.fn()
   render(<Blog
     id='12345'
     title='Test Blog 1'
@@ -14,29 +18,27 @@ test('Rendered Blog component contains correct title', () => {
     url='www.test.org'
     likes={1}
     user='Tepi Testikäyttäjä'
-    likeBlog={mockFunc}
+    likeBlog={likeMockHandler}
     addedByCurrentLoggedInUser='true'
-    removeBlog={mockFunc}
+    removeBlog={removeMockHandler}
   />)
 
   const element = screen.getByText('Test Blog 1 - by Test Writer 1')
 })
 
 test('URL, likes and user are shown correctly after view button clicked', async () => {
-  const mockFunction = () => {
-    console.log('this is a mock function')
-  }
-
+  const likeMockHandler = vi.fn()
+  const removeMockHandler = vi.fn()
   render(<Blog
     id='12345'
-    title='Test Blog 2'
-    author='Test Writer 2'
+    title='Test Blog 1'
+    author='Test Writer 1'
     url='www.test.org'
     likes={1}
     user='Tepi Testikäyttäjä'
-    likeBlog={mockFunction}
+    likeBlog={likeMockHandler}
     addedByCurrentLoggedInUser='true'
-    removeBlog={mockFunction}
+    removeBlog={removeMockHandler}
   />)
 
   const user = userEvent.setup()
@@ -46,5 +48,31 @@ test('URL, likes and user are shown correctly after view button clicked', async 
   const urlElement = screen.getByText('Url: www.test.org')
   const likesElement = screen.getByText('Likes: 1')
   const userElement = screen.getByText('User: Tepi Testikäyttäjä')
+})
 
+test('If Like button is clicked twice, mock Handler is called 2 times', async () => {
+  const likeMockHandler = vi.fn()
+  const removeMockHandler = vi.fn()
+  render(<Blog
+    id='12345'
+    title='Test Blog 1'
+    author='Test Writer 1'
+    url='www.test.org'
+    likes={1}
+    user='Tepi Testikäyttäjä'
+    likeBlog={likeMockHandler}
+    addedByCurrentLoggedInUser='true'
+    removeBlog={removeMockHandler}
+  />)
+
+  const user = userEvent.setup()
+
+  let button = screen.getByText('View')
+  await user.click(button)
+
+  button = screen.getByText('Like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(likeMockHandler.mock.calls).toHaveLength(2)
 })
