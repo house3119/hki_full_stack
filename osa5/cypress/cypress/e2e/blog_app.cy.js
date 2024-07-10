a77c824a1093c3e71983ba6d73fa1cf1991b84cd
Antti Varis <ajvari88@gmail.com>
2024-07-10 20:07:17 +0300

    teh5.18

--- osa5/cypress/cypress/e2e/blog_app.cy.js
+++ osa5/cypress/cypress/e2e/blog_app.cy.js
@@ -4,9 +4,17 @@
   })
 })*/
 
-describe('Blog App', function () {
+describe('Blog App E2E tests', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/test/reset')
+
+    const user = {
+      username: 'Teppo Testaaja 69',
+      name: 'Teppo Töppönen',
+      password: 'Erittäin salainen passu 35'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+
     cy.visit('http://localhost:5173')
   })
 
@@ -16,4 +24,31 @@ describe('Blog App', function () {
     cy.contains('Password')
     cy.contains('Login')
   })
+
+  describe('Login', function() {
+    it('Login succesful with correct username and password', function() {
+      cy.get('#username').type('Teppo Testaaja 69')
+      cy.get('#password').type('Erittäin salainen passu 35')
+      cy.contains('Login').click()
+  
+      cy.contains('Logged in as Teppo Testaaja 69')
+    })
+
+    it('Login fails with wrong username', function() {
+      cy.get('#username').type('Teppo Testaaja 7329')
+      cy.get('#password').type('Erittäin salainen passu 35')
+      cy.contains('Login').click()
+  
+      cy.contains('Invalid Username')
+    })
+  
+    it('Login fails with wrong password', function() {
+      cy.get('#username').type('Teppo Testaaja 69')
+      cy.get('#password').type('Väärä passu')
+      cy.contains('Login').click()
+  
+      cy.contains('Invalid Username')
+    })
+  })
+  
 })
